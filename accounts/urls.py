6a2c731760056e2048f28019d1165f144541f8cc
Avani Gupta <avani.gupta200367@gmail.com>
2025-05-13 18:06:41 +0530

    user authentication and form backend

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+# urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('auth/login/', views.login_user, name='login'),
+    path('auth/register/', views.register_user, name='register'),
+    path('auth/logout/', views.logout_user, name='logout'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.template import loader
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        # Validation
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists.')
+            return redirect('/auth/register/')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already registered.')
+            return redirect('/auth/register/')
+
+        if password1 != password2:
+            messages.error(request, 'Passwords do not match.')
+            return redirect('/auth/register/')
+
+        # Create user
+        user = User.objects.create_user(username=username, email=email)
+        user.first_name = fullname
+        user.set_password(password1)
+        user.save()
+
+        messages.success(request, 'User registered successfully. Please login.')
+        return redirect('/auth/login/')
+
+    template = loader.get_template('register.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('/home/problemList/')  # Change to your dashboard URL
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('/auth/login/')
+
+    template = loader.get_template('login.html')
+    context ={}
+    return HttpResponse(template.render(context,request))
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully.')
+    return redirect('/auth/login/')
