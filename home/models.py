2fbe89fe045deaea5f0d6298e3dd4156fbf16b83
Avani Gupta <avani.gupta200367@gmail.com>
2025-05-15 21:37:38 +0530

    Problems list and details

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Problem(models.Model):
+    DIFFICULTY_LEVELS = [
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Hard', 'Hard'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_LEVELS)
+    input_testcase = models.TextField()
+    output_testcase = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
--- home/views.py
+++ home/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from home.models import Problem
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+
+def problem_list(request):
+    problems = Problem.objects.all()
+    template = loader.get_template("problem_list.html")
+    context = {
+        'problems':problems,
+    }
+    return HttpResponse(template.render(context, request))
+
+def problem_detail(request, id):
+    req_problem = Problem.objects.get(id=id)
+    template = loader.get_template("problem_detail.html")
+    context = {
+        "req_problem":req_problem,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+
