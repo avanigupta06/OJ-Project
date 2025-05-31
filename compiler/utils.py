9ecf72a345a90b80c00edbb5c9080c067c8701d2
Avani Gupta <avani.gupta200367@gmail.com>
2025-05-31 17:22:05 +0530

    modifications

--- compiler/utils.py
+++ compiler/utils.py
@@ -1,32 +1,32 @@
-# import google.generativeai as genai
-# from django.conf import settings
+import google.generativeai as genai
+from django.conf import settings
 
-# def get_code_review(code, problem_desc):
-#     if not code.strip():
-#         return "You have not written any code."
+def get_code_review(code, problem_desc):
+    if not code.strip():
+        return "You have not written any code."
     
-#     genai.configure(api_key=settings.GEMINI_API_KEY)
-#     model = genai.GenerativeModel("gemini-2.0-flash")
-#     prompt = f"""
-# You are a code reviewer. Given the following problem description and code:
+    genai.configure(api_key=settings.GEMINI_API_KEY)
+    model = genai.GenerativeModel("gemini-2.0-flash")
+    prompt = f"""
+You are a code reviewer. Given the following problem description and code:
 
-# Problem:
-# {problem_desc}
+Problem:
+{problem_desc}
 
-# Code:
-# {code}
+Code:
+{code}
 
-# Provide a **short**, plain-text review (under 100 words) with:
-# - Key feedback
-# - Suggestions for improvement
-# - Mention edge cases if needed
+Provide a **short**, plain-text review (under 100 words) with:
+- Key feedback
+- Suggestions for improvement
+- Mention edge cases if needed
 
-# Do NOT include code snippets or examples. Avoid using stars, bullet points, or markdown formatting.
-#     """
+Do NOT include code snippets or examples. Avoid using stars, bullet points, or markdown formatting.
+    """
 
 
-#     try:
-#         response = model.generate_content(prompt)
-#         return response.text
-#     except Exception as e:
-#         return f"AI Review Error: {str(e)}"
+    try:
+        response = model.generate_content(prompt)
+        return response.text
+    except Exception as e:
+        return f"AI Review Error: {str(e)}"
--- compiler/views.py
+++ compiler/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 from compiler.forms import CodeSubmissionForm
 from compiler.models import CodeSubmission
 from home.models import Problem, HiddenTestCase
-# from compiler.utils import get_code_review 
+from compiler.utils import get_code_review 
 from django.conf import settings
 from pathlib import Path
 import uuid
@@ -58,8 +58,8 @@ def run_code_view(request, problem_id):
                 submission.output_data = "Accepted" if all_passed else "Rejected"
 
                 # ai review
-                # review = get_code_review(language, problem.description)
-                # submission.ai_feedback = review
+                review = get_code_review(language, problem.description)
+                submission.ai_feedback = review
 
 
                 submission.save()
